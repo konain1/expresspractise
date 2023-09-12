26f5e5ccac11aef57fbf3ba7ff2f086193eb7c23
konain1 <kaunainkasab@gmail.com>
2023-09-12 15:09:48 +0530

    middleware ownMiddleware 3rd party middleware morgan

--- app.js
+++ app.js
@@ -3,9 +3,20 @@
 const express = require('express')
 const app = express();
 const path = require('path')
+const morgan = require('morgan')
 
 
 app.use(express.static('public'))
+
+
+// const logger = (req,res,next)=>{
+//     console.log(`${new Date()} , ${req.url} , ${req.method}`)
+//     next()
+// }
+
+// app.use('/contact',logger)
+
+app.use(morgan('dev'))
 
 app.get('/',(req,res)=>{
 
@@ -25,6 +36,13 @@ app.get('/product',(req,res)=>{
     res.sendFile(path.join(__dirname, 'public' ,'product.html'))
 })
 
+app.get('/json',(req,res)=>{
+
+    res.json({
+        name:"konain",
+        school:"insan school"
+    })
+})
 app.listen(4500,()=>{
     console.log('port 4500')
 })
