0cb7ea0480a522b933a6219402fe9fa3f6e7dca9
konain1 <kaunainkasab@gmail.com>
2023-09-12 14:33:28 +0530

    routeDoone

--- app.js
+++ app.js
@@ -2,12 +2,25 @@
 
 const express = require('express')
 const app = express();
+const path = require('path')
 
 app.get('/',(req,res)=>{
 
-    res.send('hi there')
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+app.get('/contact',(req,res)=>{
+
+    res.sendFile(path.join(__dirname, 'contact.html'))
+})
+app.get('/service',(req,res)=>{
+
+    res.sendFile(path.join(__dirname, 'service.html'))
 })
 
+app.get('/product',(req,res)=>{
+
+    res.sendFile(path.join(__dirname, 'product.html'))
+})
 
 app.listen(4500,()=>{
     console.log('port 4500')
