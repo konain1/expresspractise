fc6bbd7bd8187212e08eac4811dd2940e3eac3d5
konain1 <kaunainkasab@gmail.com>
2023-09-12 14:42:37 +0530

    middleware app.use(express.static(public)

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ const express = require('express')
 const app = express();
 const path = require('path')
 
+
+app.use(express.static('public'))
+
 app.get('/',(req,res)=>{
 
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 app.get('/contact',(req,res)=>{
 
-    res.sendFile(path.join(__dirname, 'contact.html'))
+    res.sendFile(path.join(__dirname,'public' , 'contact.html'))
 })
 app.get('/service',(req,res)=>{
 
-    res.sendFile(path.join(__dirname, 'service.html'))
+    res.sendFile(path.join(__dirname, 'public' ,'service.html'))
 })
 
 app.get('/product',(req,res)=>{
 
-    res.sendFile(path.join(__dirname, 'product.html'))
+    res.sendFile(path.join(__dirname, 'public' ,'product.html'))
 })
 
 app.listen(4500,()=>{
